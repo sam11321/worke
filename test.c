13948803c5fbd942bafa679fc9c41ce6fdcc55fe
(Sam) <samwu1112@gmai.coml)>
2022-10-02 15:50:16 +0800

    type rfi

--- test.c
+++ test.c
@@ -2,11 +2,11 @@
 
 int main()
 { 
-    int a = 0 ,b = 0, sum, product, d ,t;
+    int a = 0 ,b = 0, sum, product, d;
     printf("Enter two integer: ");
     scanf(" %d %d" ,&a,&b);
     sum = a + b;
     printf("Sum a is: %d\n " ,sum);
     
-}
+} 
     

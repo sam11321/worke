213ab1494acf3696399120b9f59607f44cea04c5
(Sam) <samwu1112@gmai.coml)>
2022-10-02 15:30:22 +0800

    type fuck

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+
+int main()
+{ 
+    int a = 0 ,b = 0, sum, product, d ,t;
+    printf("Enter two integer: ");
+    scanf(" %d %d" ,&a,&b);
+    sum = a + b;
+    printf("Sum a is: %d\n " ,sum);
+    
+}
+    

39118aaa663b003237e134646a293c3bff28d7f3
(Sam) <samwu1112@gmai.coml)>
2022-10-03 14:08:41 +0800

    type change

--- homework3.c
+++ homework3.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main(){
     double v, u, a, s, t;
-    // 輸入速度、加速度、時間
+   
     printf("Enter initial velocity: ");
     scanf("%lf",&u);
     printf("Enter initial acceleration: ");
     scanf("%lf",&a);
     printf("Enter initial time: ");
     scanf("%lf",&t);
-    //
+    
     if (t >= 0){ 
         v = u + (a*t);
-        s = (u*t) + (t*t*2)/2;
+        s = (u*t) + (t*t*a)/2;
         printf("V is%lf\n",v);
         printf("S is%lf",s);
     }
@@ -26,7 +26,9 @@ int main(){
         printf(" You enter wrong value.Please rewrite the time\n");
         scanf("%lf", &t);
         v = u + (a*t);
-        s = (u*t) + (t*t*2)/2;
-        return (v = u + (a*t),s = (u*t) + (t*t*2)/2) ;
+        s = (u*t) + (t*t*a)/2;
+        printf("V is%lf\n",v);
+        printf("S is%lf",s);
+        return 0 ;
     }
 }
